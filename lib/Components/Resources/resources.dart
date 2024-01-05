433d39ec809729cd8cd2cb7194a73a64c36b19d4
Muhammadjj <mrjawad933@gmail.com>
2024-01-05 05:19:30 -0800

    changes

--- lib/Components/Resources/resources.dart
+++ lib/Components/Resources/resources.dart
@@ -1,5 +1,9 @@
 import 'package:e_commerce/Components/Resources/app_colors.dart';
+import 'package:e_commerce/Components/Resources/images_path.dart';
+import 'package:e_commerce/Components/Resources/text_style.dart';
 
 class Resources {
   static AppColors colors = AppColors();
+  static ImagePath imagePath = ImagePath();
+  static AppTextStyle textStyle = AppTextStyle();
 }
